07ff8b284bea07d65fdf17c786dd22dd895172fd
Kota Aoyama <16607351+KotaAoyama@users.noreply.github.com>
2026-03-27 18:12:38 +0900

    chore: first commit

---
+++ src/test/java/com/kota/approvalworkflowapi/ApprovalWorkflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kota.approvalworkflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalWorkflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
