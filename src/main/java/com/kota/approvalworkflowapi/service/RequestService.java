e9b42944647107c4240fba0a2e741ab4ca7475f3
Kota Aoyama <16607351+KotaAoyama@users.noreply.github.com>
2026-04-03 18:45:45 +0900

    feat: implement createRequest in RequestService

---
+++ src/main/java/com/kota/approvalworkflowapi/service/RequestService.java
@@ -0,0 +1,47 @@
+package com.kota.approvalworkflowapi.service;
+
+import com.kota.approvalworkflowapi.common.RequestStatus;
+import com.kota.approvalworkflowapi.dto.RequestInput;
+import com.kota.approvalworkflowapi.dto.RequestSummary;
+import com.kota.approvalworkflowapi.entity.RequestEntity;
+import com.kota.approvalworkflowapi.repository.RequestRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class RequestService {
+
+    // Phase1では固定値
+    private static final String USER_ID = "userId1234";
+    private static final String USER_NAME = "user1";
+
+    private final RequestRepository requestRepository;
+
+    public RequestService(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    public RequestSummary createRequest(RequestInput input) {
+        String requestId = UUID.randomUUID().toString();
+        LocalDateTime now = LocalDateTime.now();
+        RequestEntity savedEntity = requestRepository.saveRequest(
+                RequestEntity.builder()
+                        .requestId(requestId)
+                        .userId(USER_ID)
+                        .status(RequestStatus.DRAFT)
+                        .title(input.getTitle())
+                        .description(input.getDescription())
+                        .createdAt(now)
+                        .updatedAt(now)
+                        .build());
+        return RequestSummary.builder()
+                .requestId(savedEntity.getRequestId())
+                .userName(USER_NAME)
+                .status(savedEntity.getStatus())
+                .title(savedEntity.getTitle())
+                .createdAt(savedEntity.getCreatedAt())
+                .build();
+    }
+}
