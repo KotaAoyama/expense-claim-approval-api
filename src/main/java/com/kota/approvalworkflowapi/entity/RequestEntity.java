2365e0e61a2ffc475a5e6394eb22772e2bf0d671
Kota Aoyama <16607351+KotaAoyama@users.noreply.github.com>
2026-04-05 23:06:13 +0900

    refactor: add JPA annotations to RequestEntity

--- src/main/java/com/kota/approvalworkflowapi/entity/RequestEntity.java
+++ src/main/java/com/kota/approvalworkflowapi/entity/RequestEntity.java
@@ -1,19 +1,32 @@
 package com.kota.approvalworkflowapi.entity;
 
 import com.kota.approvalworkflowapi.common.RequestStatus;
-import lombok.Builder;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "requests")
 public class RequestEntity {
+
+    @Id
     private String requestId;
+
     private String userId;
+
+    @Enumerated(EnumType.STRING)
     private RequestStatus status;
+
     private String title;
+
     private String description;
+
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
 }
